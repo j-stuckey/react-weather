ce524826d9b184e937a46b13e294280a76d4c7c5
James Stuckey <jstuckey@rutgers.edu>
2020-08-02 18:06:52 -0400

    UI change for % precip

--- src/components/WeatherCard/index.js
+++ src/components/WeatherCard/index.js
@@ -4,7 +4,14 @@ import './WeatherCard.css';
 import Moment from 'react-moment';
 
 function WeatherCard(props) {
-    const { time, temperatureHigh, temperatureLow, icon, precipProbability, precipType } = props.data;
+    const {
+        time,
+        temperatureHigh,
+        temperatureLow,
+        icon,
+        precipProbability,
+        precipType
+    } = props.data;
 
     return (
         <div className="WeatherCard">
@@ -16,9 +23,26 @@ function WeatherCard(props) {
                 {Math.round(temperatureHigh)} º F / {Math.round(temperatureLow)}{' '}
                 º F
             </p>
-            <p>{Math.round(precipProbability * 100)} % {precipType}</p>
+
+            {icon === 'rain' ? (
+                <Precipitation
+                    precipProbability={Math.round(precipProbability * 100)}
+                    type={precipType}
+                />
+            ) : null}
         </div>
     );
 }
+
+const Precipitation = ({ precipProbability, type: precipType }) => {
+    if (precipProbability > 10) {
+        return (
+            <p>
+                {precipProbability} %{' '}
+                <span className="precipitation">{precipType}</span>
+            </p>
+        );
+    } else return null;
+};
 
 export default WeatherCard;
